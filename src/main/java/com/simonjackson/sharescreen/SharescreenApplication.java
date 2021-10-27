31bbbfad1b002e9bc632d024c469b9430ae0178f
SimonJackson <1101811894@qq.com>
2021-10-27 16:55:32 +0800

    first commit

---
+++ src/main/java/com/simonjackson/sharescreen/SharescreenApplication.java
@@ -0,0 +1,13 @@
+package com.simonjackson.sharescreen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharescreenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SharescreenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/simonjackson/sharescreen/SharescreenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simonjackson.sharescreen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharescreenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
